e93a19f83ea42b5a4dcc358039002270a0b9c360
razmyslovartem <razmyslovartem@icloud.com>
2026-03-20 12:23:40 +0300

    update test_api_clients.py

--- tests/test_api_clients.py
+++ tests/test_api_clients.py
@@ -8,23 +8,6 @@ import src.api_clients as api_clients
 from src.api_clients import APIAdapter
 
 
-@pytest.mark.parametrize(
-    "url, order_codes",
-    [
-        ("https://httpbin.org/status/200", [200]),
-        ("https://opensky-network.org", [200, 403]),
-        ("https://nominatim.openstreetmap.org", [200, 403]),
-    ],
-)
-def test_multiple_api_nodes(url, order_codes):
-    """Параметризованный тест для проверки нескольких API‑узлов"""
-    try:
-        response = requests.get(url, timeout=10)
-        assert response.status_code in order_codes, f"Узел {url} вернул статус: {response.status_code}. "
-    except requests.exceptions.RequestException as e:
-        pytest.fail(f"Не удалось подключиться к {url}: {e}")
-
-
 class MockResponse:
     def __init__(self, json_data, status_code=200):
         self._json_data = json_data
